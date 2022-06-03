8eaab69ea8b8b97bbc51a625ddf6470dcd25aded
isakbet <yosephbet@gmail.com>
2022-06-03 19:27:23 +0200

    Import config, dbconnect, header & footer.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Shop</title>
-</head>
-<body>
-	<h1>Shop page</h1>
-</body>
-</html>
+<?php
+    require('../src/config.php');
+    require('../src/dbconnect.php');
+?>
+
+<?php include('layout/header.php'); ?>
+
+	<div class="container-fluid indexPage">
+		<div class="d-flex flex-column justify-content-center logoContainer">
+			<img class="logo" src="img/babysme_logo.png" alt="" style="width: 65vh;">
+			<button class="btn shopBtn align-self-center" style="width:10em;"><a class="indexBtn" href="products.php">Shop now</a></button>
+		</div>
+	</div>
+
+<?php include('layout/footer.php'); ?>
