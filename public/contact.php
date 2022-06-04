653c206f381ceb8b0ada02263f1243613c0b42dc
isakbet <yosephbet@gmail.com>
2022-06-04 08:27:33 +0200

    Add contact.php

---
+++ public/contact.php
@@ -0,0 +1,27 @@
+<?php
+    require('../src/config.php');
+?>	
+    
+    <?php include('layout/header.php'); ?>
+    
+    <div class="container-fluid p-0"  id="productPage">
+        <div class="d-flex flex-column">
+            <div class="d-flex justify-content-center mt-5">
+                <div class="col text-center">
+                    <h4>Contact Us</h4>
+                </div>      
+            </div>
+
+            <div class="d-flex innerContactPage px-5">
+                <div class="col-7">
+
+
+                </div>
+                <div class="col-5">
+                    <img src="../public/img/contact-bg.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include('layout/footer.php'); ?>
