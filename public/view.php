7161a818886993fce60b4cfa0700b303034cd0a1
isakbet <yosephbet@gmail.com>
2022-06-04 17:19:24 +0200

    Edit deleteBtn

---
+++ public/view.php
@@ -0,0 +1,77 @@
+<?php
+    require('../src/dbconnect.php');
+    require('../src/config.php');
+
+    if (isset($_POST['deleteBtn'])) {
+        $id = $_POST['id'];
+        $query = "DELETE FROM products WHERE id = :id";
+        $stmt = $dbconnect->prepare($query);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+    }
+    
+    $title          = '';
+    $description    = '';
+    $price          = '';
+    $error          = '';
+    $msg            = '';
+    $id             = $_GET["id"];
+
+    if (isset($_POST['send'])) {
+        $title = trim($_POST['title']);
+        $description = trim(substr(($_POST['description']), 0, 10));
+        $price = trim($_POST['price']);
+
+        if (empty($error)) {
+            try {
+                $query = "
+                UPDATE products
+                SET title = :title, description = :description, price = :price
+                WHERE id = :id
+                ";
+
+                $stmt = $dbconnect->prepare($query);
+                $stmt->bindValue(':title', $title);
+                $stmt->bindValue(':description', $description);
+                $stmt->bindValue(':price', $price);
+                $stmt->execute();
+            } catch (\PDOException $e) {
+                throw new \PDOException($e->getMessage(), (int) $e->getCode());
+            }
+        }
+    }
+    
+    $product = $productDbHandler -> fetchAllProducts($id);    
+?>
+
+    <?php include('layout/header.php'); ?>
+
+    <div class="d-flex flex-column mt-5 ml-5" id="productPage">
+        <form action="#" method="POST">
+            <div class="d-flex justify-content-center">
+                <div class="col-3">
+                    <image src="admin/<?=$product['image_url']?>" style="width:90px;height:auto;">
+                </div>
+
+                <div class="d-flex flex-column">
+                    <div class="col">
+                        <h1><?=htmlentities($product['title'])?></h1> <br>
+                        <p><?=htmlentities($product['description'])?></p>
+                    </div>
+                
+                    <div class="col">
+                        <h3 class="font-weight-bold"><?=htmlentities($product['price'])?>SEK</h3>
+                    </div>
+                </div>
+            </div>   
+        </form>
+
+        <div class="d-flex justify-content-center">
+            <form action="products.php">
+                <input type="submit" class="form-control btn-dark btn text-light mx-auto" value="Back to products">
+            </form>
+        </div>
+    </div>
+
+
+<?php include('layout/footer.php'); ?>
