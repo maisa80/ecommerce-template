41a768cf0e88abf5d003ec16070ddf56df5594bf
isakbet <yosephbet@gmail.com>
2022-06-04 17:17:30 +0200

    add-cart-item.php

---
+++ public/add-cart-item.php
@@ -0,0 +1,48 @@
+<?php
+    require('../src/config.php');
+    require('../src/dbconnect.php');
+
+    echo"<pre>";
+    print_r($_POST);
+    echo"<pre>";
+    
+    if(!empty($_POST['quantity'])) {
+        $articleId = (int) $_POST['articleId'];
+        $quantity = (int) $_POST['quantity'];
+       
+        try{
+            $query = "
+                SELECT * FROM products
+                WHERE id = :id;
+                ";
+            $stmt = $dbconnect->prepare($query);
+            $stmt->bindValue(':id', $articleId);
+            $stmt->execute();
+            $article = $stmt->fetch();
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int) $e->getCode());
+        }
+        
+        if ($article) {
+            $article = array_merge($article, ['quantity' => $quantity]);
+             echo"<pre>";
+             print_r($article);
+             echo"<pre>";
+        
+            $articleItem = [$articleId => $article];
+        }
+
+        if (empty($_SESSION['items'])) {
+            $_SESSION['items'] = $articleItem;
+        } else {
+            if (isset($_SESSION['items'][$articleId])) {
+                $_SESSION['items'][$articleId]['quantity'] += $quantity;
+            } else {
+                $_SESSION['items'] += $articleItem;
+            }
+        }
+    }
+
+    header('Location: products.php');
+    exit;
+?>
