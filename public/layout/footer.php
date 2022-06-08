83d9220f7c57d945a8a2583e67037203000d51a4
isakbet <yosephbet@gmail.com>
2022-06-08 20:16:58 +0200

    Remove list-item from footer

--- public/layout/footer.php
+++ public/layout/footer.php
@@ -15,8 +15,8 @@
                               <li><a href="index.php">About us</a></li>
                               <li><a href="#">Faq's</a></li>
                               <li><a href="contact.php">Contact us</a></li>
-                              <li><a href="#">Membership</a></li>
-                              <li><a href="#">Blog</a></li>
+                              <li><a href="#">Terms & Conditions</a></li>
+                              <li><a href="#">Privacy</a></li>
                          </ul>
                     </div>
                </div>
@@ -24,10 +24,9 @@
                     <div class="footer-col last">
                          <h4>Follow us</h4>
                          <ul class="list-inline">
-                              <li><a href="https://www.facebook.com/babysmec/"><i class="fa fa-facebook"></i></a></li>
+                              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                              <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                              <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                              <li><a href="#"><i class="fa fa-google-plus"></i></a></li>                             
                               <li><a href="https://www.instagram.com/babysmec/"><i class="fa fa-instagram"></i></a></li>
                               <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
                          </ul>
@@ -44,6 +43,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/js/fontawesome.min.js"></script>
 
 <!-- CUSTOM JavaScript -->
 <script src="js/main.js"></script>
