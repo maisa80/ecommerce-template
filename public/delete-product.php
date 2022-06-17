3ec2f56e37f2fddc78c0d58b8a440a6f0ed47fe9
isakbet <yosephbet@gmail.com>
2022-06-17 09:00:34 +0200

    Add delete-product.php

---
+++ public/delete-product.php
@@ -0,0 +1,22 @@
+<?php
+
+    require('../src/dbconnect.php');
+    require('../src/config.php');
+
+    if (isset($_POST['deleteProductBtn'])) {
+      $id = $_POST['id'];
+      $productDbHandler->deleteProduct($id);
+    }
+    
+    $products = $productDbHandler->fetchAllProducts();
+
+    $data = [
+
+        'products' => $products,
+        
+    ];
+
+    echo json_encode($data);
+
+
+?>
