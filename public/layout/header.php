f3173a25d3f15fd07bc513aabc2bb60f719f75ab
isakbet <yosephbet@gmail.com>
2022-06-17 19:31:58 +0200

    Remove Dashboard, Admin links

--- public/layout/header.php
+++ public/layout/header.php
@@ -7,60 +7,44 @@
 
     <!-- Bootstrap CSS -->
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/a97da01e51.js" crossorigin="anonymous"></script>
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/style.css">
 
-    <title><?=$pageTitle ?></title>
+    <title><?= $pageTitle ?></title>
 </head>
 
-<body id="<?=$pageId ?>">
+<body id="<?= $pageId ?>">
 
-    <div class="container-fluid p-0">
-        <!-- Log in/ Log out -->
-
+    <div class="container-fluid p-0 m-0">
         <div class="header">
             <div id="logo">
                 <img class="logo" src="img/babysme_logo.png" alt="logo">
             </div>
             <div id="topmenu">
                 <?php
-						if (isset($_SESSION['username'])) {
-							
-						// ucfirst makes the first letter to a CAPITAL letter :)
-						$loggedInUsername = htmlentities(ucfirst($_SESSION['username']));
-						$loggedInUserId = htmlentities($_SESSION['id']);
-						$aboveNave = "<span class='logInBtn'><b>Hi {$loggedInUsername}</b></span>
+                if (isset($_SESSION['username'])) {
+                    // ucfirst makes the first letter to a CAPITAL letter :)
+                    $loggedInUsername = htmlentities(ucfirst($_SESSION['username']));
+                    $loggedInUserId = htmlentities($_SESSION['id']);
+                    $aboveNave = "<span class='logInBtn'><b>Hi {$loggedInUsername}</b></span>
 						<a id='mypages' class='logInBtn' href='my-pages.php?userId=$loggedInUserId'><i class='fas fa-user-alt'></i> My account</a>
 						<a href='logout.php' class='logInBtn' ><i class='fas fa-sign-out-alt'></i> Log out</a>";
-						} else {
-						$aboveNave = "<a href='register.php' class='logInBtn'><i class='fas fa-user'></i> Sign up</a> 
+                } else {
+                    $aboveNave = "<a href='register.php' class='logInBtn'><i class='fas fa-user'></i> Sign up</a> 
 						<a href='login.php' class='logInBtn'><i class='fas fa-sign-in-alt'></i> Log in</a>";
-						}
-
-						echo $aboveNave;
-						
-					?>
+                }
+                echo $aboveNave;
+                ?>
             </div>
         </div>
-        <!-- <a id="dashboard"  class="logInBtn" href="admin/index.php">Dashboard</a> -->
 
-    </div>
-    <!-- <form action="my-pages.php?" method="GET">
-					<input type="hidden" name="id" value="">
-					<input type="submit" value="My page" class="btn logInBtn">
-				</form>
+   
 
-				<form action="admin/admin.php?">
-					<input type="submit" value="Admin" class="btn logInBtn">
-				</form>
-			</div> -->
     <!-- Navbar -->
     <div class="d-flex justify-content-center text-center bg-transparent pb-4">
-
         <div class="col">
             <form action="index.php?">
                 <input type="submit" value="Home" class="btn navBtn">
@@ -75,10 +59,9 @@
             <form action="contact.php?">
                 <input type="submit" value="Contact" class="btn navBtn">
             </form>
-        </div>
-
-
+        </div>    
+    <div class="cart-col">
+        <?php include('cart.php') ?>
     </div>
-    <div class="d-flex justify-content-end mr-2">
-        <?php include('cart.php')?>
     </div>
+    
