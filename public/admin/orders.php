5b6101854ba796df86d5a8bcf8af0c9f02de65d3
Maisa Abusalem <94901967+maisa80@users.noreply.github.com>
2022-06-23 09:45:10 +0200

    add dropdownlist admin/orders

--- public/admin/orders.php
+++ public/admin/orders.php
@@ -5,48 +5,59 @@
     $message= '';
     $orders = $orderDbHandler->fetchAllOrders();
  ?>
- 
- <?php include('layout/header.php'); ?>
 
- <!-- Sidans/Dokumentets huvudsakliga innehåll -->
- <div id="content">
- 
-     <h4>Manage users </h4>
- 
-     <br>
-     <?=$message ?>
-     <p>Total: <span>( <?=count($orders); ?> ) orders</span></p>
-     <table class="table ">
-         <thead>
-             <tr>
-                 <th>Order Id</th>
-                 <th>Customer name</th>
-                 <th>Price</th>
-                 <th>Order date</th>
-                 <th class="manage">Manage status</th>
-             </tr>
-         </thead>
-         <tbody>
-             <?php foreach($orders as $order) : ?>
-             <tr>
-                 <td>
-                 <form action="order.php" method="GET">
+<?php include('layout/header.php'); ?>
+
+<!-- Sidans/Dokumentets huvudsakliga innehåll -->
+<div id="content">
+
+    <h4>Manage users </h4>
+
+    <br>
+    <?=$message ?>
+    <p>Total: <span>( <?=count($orders); ?> ) orders</span></p>
+    <table class="table ">
+        <thead>
+            <tr>
+                <th>Order Id</th>
+                <th>Customer name</th>
+                <th>Price</th>
+                <th>Order date</th>
+                <th class="manage">Manage status</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($orders as $order) : ?>
+            <tr>
+                <td>
+                    <form action="order.php" method="GET">
                         <input type="hidden" name="orderId" value="<?=htmlentities($order['id']) ?>">
                         <button type="submit" class="btn btn-warning">#<?=htmlentities($order['id']) ?></button>
-                    </form> 
-                 </td>
-                 <td><?=htmlentities($order['billing_full_name']) ?></td>
-                 <td><?=htmlentities($order['total_price']) ?></td>
-                 <td><?=htmlentities($order['create_date']) ?></td>
-                 <td class="manage">
-                     
-                 </td>
- 
-             </tr>
-             <?php endforeach; ?>
-         </tbody>
-     </table>
- 
- </div>
- 
- <?php include('layout/footer.php'); ?>
+                    </form>
+                </td>
+                <td><?=htmlentities($order['billing_full_name']) ?></td>
+                <td><?=htmlentities($order['total_price']) ?></td>
+                <td><?=htmlentities($order['create_date']) ?></td>
+                <td class="manage">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Action
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Pending</a></li>
+                            <li><a class="dropdown-item" href="#">Processing</a></li>
+                            <li><a class="dropdown-item" href="#">Completed</a></li>
+
+                        </ul>
+                    </div>
+                </td>
+
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+</div>
+
+<?php include('layout/footer.php'); ?>
